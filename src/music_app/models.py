a513cc81b1f90f8d65a273711227375c5e58f507
Deepak Sharnma <deepaks4077@gmail.com>
2016-03-23 17:47:33 -0400

    Added history model

--- src/music_app/models.py
+++ src/music_app/models.py
@@ -26,6 +26,17 @@ class Song(models.Model):
         return self.link
 
 @python_2_unicode_compatible
+class History(models.Model):
+    link = models.CharField(max_length=44)
+    add_time = models.DateTimeField("time song was added")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    #user_ip = models.CharField(max_length=45)
+
+    #print "%s -- %s" % (title, author)
+    def __str__(self):
+        return self.link
+
+@python_2_unicode_compatible
 class User(models.Model):
     ip_address = models.PositiveIntegerField(max_length=12)
     songs_added = models.PositiveIntegerField(max_length=3)
